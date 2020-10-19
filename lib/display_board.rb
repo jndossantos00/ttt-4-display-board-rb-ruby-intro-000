45297391af7ebfd264cfda5e0b73c8911bc6e472
Jessica dos Santos <jndossantos00@gmail.com>
2020-10-19 19:11:33 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,15 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+def display_board(board)
+
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+
+puts "#{board[0]},X: #{board[1]},#{board[2]}"
+puts "O: #{board[3]},#{board[4]},#{board[5]}"
+puts "X: #{board[6]},O: #{board[7]},#{board[8]}"
+
+
+
+end
