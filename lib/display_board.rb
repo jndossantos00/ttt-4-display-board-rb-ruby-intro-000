827c24661e2259f08612a09d06c259fe4dd66650
Jessica dos Santos <jndossantos00@gmail.com>
2020-10-19 19:21:16 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -6,7 +6,7 @@ def display_board(board)
   puts "-----------"
   puts "   |   |   "
 
-puts "#{board[0]},"": #{board[1]}," "#{board[2]},O: #{board[3]},#{board[4]},#{board[5]},"": #{board[6]},O: #{board[7]},#{board[8]}"
+puts "#{board[0]}," ": #{board[1]}," "#{board[2]},O: #{board[3]},#{board[4]},#{board[5]},"": #{board[6]},O: #{board[7]},#{board[8]}"
 
 
 
